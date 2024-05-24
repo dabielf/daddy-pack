0b8919825b2d32b9624ac16192703d9b5b95e51c
François Dabiel <dabielf@gmail.com>
2024-05-24 08:22:28 -0400

    commented contactList for now

--- components/blocks/contactList.tsx
+++ components/blocks/contactList.tsx
@@ -4,22 +4,22 @@ import { formatRelative } from "date-fns";
 import Link from "next/link";
 
 export function ContactList() {
-  const { contacts, daddies } = useConvexData();
+  // const { contacts, daddies } = useConvexData();
 
-  const contactsDaddyIdSet = new Set();
-  contacts?.map((contact) => contactsDaddyIdSet.add(contact.daddy));
+  // const contactsDaddyIdSet = new Set();
+  // contacts?.map((contact) => contactsDaddyIdSet.add(contact.daddy));
 
-  const daddiesMap = new Map();
-  daddies?.map((daddy) => daddiesMap.set(daddy._id, daddy));
+  // const daddiesMap = new Map();
+  // daddies?.map((daddy) => daddiesMap.set(daddy._id, daddy));
 
-  if (!contacts) return null;
-  if (!daddies) return null;
+  // if (!contacts) return null;
+  // if (!daddies) return null;
 
-  const orderedContacts = contacts.sort((a, b) => b.date - a.date);
+  // const orderedContacts = contacts.sort((a, b) => b.date - a.date);
 
-  const filteredContacts = orderedContacts.filter((contact) =>
-    daddiesMap.has(contact.daddy),
-  );
+  // const filteredContacts = orderedContacts.filter((contact) =>
+  //   daddiesMap.has(contact.daddy),
+  // );
 
   function formatDate(date: number) {
     return formatRelative(new Date(date), new Date());
@@ -28,7 +28,7 @@ export function ContactList() {
   return (
     <div>
       <h1>Contact List</h1>
-      <ul>
+      {/* <ul>
         {filteredContacts.map((contact) => (
           <li key={contact._id} className="hover:underline">
             <Link href={`/contacts/${contact._id}`}>
@@ -36,7 +36,7 @@ export function ContactList() {
             </Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
